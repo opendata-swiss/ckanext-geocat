6df89d3921bb96f521a601521483fda5c72eaad4
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2017-12-21 14:08:13 +0100

    First draft of geocat harvester test

--- ckanext/geocat/tests/test_harvester.py
+++ ckanext/geocat/tests/test_harvester.py
@@ -1,5 +1,36 @@
 """Tests for harvester """
-import ckanext.geocat.harvester as harvester
+from nose.tools import assert_equal, assert_raises, assert_in
+import httpretty
 
-def test_plugin():
-    pass
+import GeocatHarvester from ckanext.geocat.harvester
+
+from ckan.tests.helpers import reset_db, call_action
+
+from ckan import model
+from ckan.plugins import toolkit
+
+from ckanext.harvest.tests.factories import (HarvestSourceObj, HarvestJobObj,
+                                             HarvestObjectObj)
+from ckanext.harvest.tests.lib import run_harvest
+
+class TestGeocatHarvester(object):
+    @classmethod
+    def setup(cls):
+        reset_db()
+        harvest_model.setup()
+
+    def test_harvest(self):
+
+        httpretty.register_uri(httpretty.GET, url,
+                               body=content, content_type=content_type)
+
+        results = run_harvest(
+            url='http://my-test-geocat.test/csw' 
+            harvester=GeocatHarvester())
+
+        result = results['dataset1-id']
+        assert_equal(result['state'], 'COMPLETE')
+        assert_equal(result['report_status'], 'added')
+        assert_equal(result['dataset']['name'], 'Test')
+        assert_equal(result['errors'], [])
+
