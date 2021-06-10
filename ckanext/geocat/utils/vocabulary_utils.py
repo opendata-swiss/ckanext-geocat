2523d39551b4d2a7fa05ace91b38041592652fe8
sabinem <sabine.maennel@gmail.com>
2021-06-10 12:46:54 +0200

    fix travis formatting error

--- ckanext/geocat/utils/vocabulary_utils.py
+++ ckanext/geocat/utils/vocabulary_utils.py
@@ -14,10 +14,7 @@ namespaces = {
 }
 
 __location__ = \
-    os.path.realpath(
-        os.path.join(os.getcwd(),
-        os.path.dirname(__file__))
-    )
+    os.path.realpath(os.path.join(os.getcwd(),os.path.dirname(__file__)))
 
 
 def get_elem_tree_from_string(xml_string):
