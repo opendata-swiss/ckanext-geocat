8433d833577b59acc9bb9355213f5b0748bf6c7b
Maennel <sabine.maennel@liip.ch>
2022-01-14 13:58:43 +0100

    correct organization link in test

--- ckanext/geocat/tests/test_dataset_metadata.py
+++ ckanext/geocat/tests/test_dataset_metadata.py
@@ -107,7 +107,7 @@ class TestGeocatDcatDatasetMetadata(unittest.TestCase):
         publisher = json.loads(dataset['publisher'])
         self.assertTrue(isinstance(publisher, dict))
         self.assertEquals(u'Bundesamt f\xfcr Umwelt', publisher['name'])
-        self.assertEquals(u'https://organization/swisstopo', publisher['url'])
+        self.assertEquals(u'https://opendata.swiss/organization/swisstopo', publisher['url'])
 
         # contact points
         self.assertTrue(hasattr(dataset['contact_points'], '__iter__'))
