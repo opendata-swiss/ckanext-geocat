f395ab1f0e2e210e0e5b59e37a20ec41d4ab286b
Sarah Hilse <sarah.hilse@liip.ch>
2023-09-29 16:15:38 +0200

    fix: set deprecated geocat format as default value, as long as the geocat format are not mapped on iana/eu vocabulary

--- ckanext/geocat/utils/ogdch_map_utils.py
+++ ckanext/geocat/utils/ogdch_map_utils.py
@@ -278,3 +278,4 @@ def _map_geocat_resource_format_to_valid_format(geocat_format):
     for key, value in valid_media_types.items():
         if geocat_format == key:
             return value
+    return geocat_format    
