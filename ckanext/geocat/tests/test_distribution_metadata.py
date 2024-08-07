b0bf71b1814e0a2883fc542c7ad16c32fe0e4bc1
kovalch <nataliia.kovalchuk@liip.ch>
2024-08-07 13:57:57 +0200

    fix: Update tests with correct term of use Values

--- ckanext/geocat/tests/test_distribution_metadata.py
+++ ckanext/geocat/tests/test_distribution_metadata.py
@@ -58,8 +58,8 @@ class TestGeocatDeprecatedDistributionProtocols(TestGeocatDistributionProtocols)
     def test_resources_are_picked_up_correctly_with_dataset_fields(self):
         self.assertEquals(4, len(self.distributions))
         for distribution in self.distributions:
-            self.assertEquals(distribution.get('rights'), "NonCommercialAllowed-CommercialAllowed-ReferenceNotRequired")
-            self.assertEquals(distribution.get('license'), "NonCommercialAllowed-CommercialAllowed-ReferenceNotRequired")
+            self.assertEquals(distribution.get('rights'), "https://opendata.swiss/terms-of-use/#terms_open")
+            self.assertEquals(distribution.get('license'), "https://opendata.swiss/terms-of-use/#terms_open")
             self.assertEquals(distribution.get('issued'), self.dataset.get('issued'))
             self.assertEquals(distribution.get('modified'), self.dataset.get('modified'))
             self._is_multi_lang(distribution['title'])
@@ -111,8 +111,8 @@ class TestGeocatNormedDistributionProtocols(TestGeocatDistributionProtocols):
     def test_fields_that_come_from_the_dataset(self):
         self.assertEquals(6, len(self.distributions))
         for distribution in self.distributions:
-            self.assertEquals(distribution.get('rights'), "NonCommercialAllowed-CommercialAllowed-ReferenceRequired")
-            self.assertEquals(distribution.get('license'), "NonCommercialAllowed-CommercialAllowed-ReferenceRequired")
+            self.assertEquals(distribution.get('rights'), "https://opendata.swiss/en/terms-of-use/#terms_by")
+            self.assertEquals(distribution.get('license'), "https://opendata.swiss/en/terms-of-use/#terms_by")
             self.assertEquals(distribution.get('issued'), self.dataset.get('issued'))
             self.assertEquals(distribution.get('modified'), self.dataset.get('modified'))
             self._is_multi_lang(distribution['title'])
